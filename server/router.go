dcb9ed22e8a6d28216f357c8e249df41cd16d733
ttbug <ttbuge@gmail.com>
2019-10-06 09:46:11 +0800

    giligili第一次代码提交

---
+++ server/router.go
@@ -0,0 +1,47 @@
+package server
+
+import (
+	"giligili/api"
+	"giligili/middleware"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+// NewRouter 路由配置
+func NewRouter() *gin.Engine {
+	r := gin.Default()
+
+	// 中间件, 顺序不能改
+	r.Use(middleware.Session(os.Getenv("SESSION_SECRET")))
+	r.Use(middleware.Cors())
+	r.Use(middleware.CurrentUser())
+
+	// 路由
+	v1 := r.Group("/api/v1")
+	{
+		v1.POST("ping", api.Ping)
+
+		// 用户登录
+		v1.POST("user/register", api.UserRegister)
+
+		// 用户登录
+		v1.POST("user/login", api.UserLogin)
+
+		v1.POST("videos", api.CreateVideo)
+		// v1.GET("video/:id", api.ShowVideo)
+		// v1.GET("videos", api.ListVideo)
+		// v1.PUT("video/:id", api.UpdateVideo)
+		// v1.DELETE("video/:id", api.DeleteVideo)
+
+		// 需要登录保护的
+		v1.Use(middleware.AuthRequired())
+		{
+			// User Routing
+			v1.GET("user/me", api.UserMe)
+			v1.DELETE("user/logout", api.UserLogout)
+		}
+
+	}
+	return r
+}
